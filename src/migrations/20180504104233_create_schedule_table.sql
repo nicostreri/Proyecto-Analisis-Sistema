6840c7ee74e293caded435a2d975a4c46f9eb79a
Pablo <pabloivan018@gmail.com>
2018-05-04 11:50:34 -0300

    BD actualizacion

--- src/migrations/20180504104233_create_schedule_table.sql
+++ src/migrations/20180504104233_create_schedule_table.sql
@@ -1,6 +1,6 @@
 create table schedules(
 	id integer auto_increment primary key,
-	nombre_fechasas varchar(30),
+	nombre_fechas varchar(30),
 	fixture_pertenece int(11),
 	constraint ref_fixture foreign key (fixture_pertenece) references fixtures (id),
 	create_at DATETIME,
--- src/migrations/20180504105642_create_match_table.sql
+++ src/migrations/20180504105642_create_match_table.sql
@@ -2,8 +2,11 @@ create table matches(
 	id integer auto_increment primary key,
 	fecha datetime,
 	schedule_pertenece integer,
-
+	team_visitante integer,
+	team_local integer,
 	constraint ref_schedule foreign key (schedule_pertenece) references schedules (id),
+	constraint ref_teams foreign key (tems_visitante) references teams (id),
+	constraint ref_teams foreign key (team_local ) references teams(id),
 	create_at DATETIME,
 	update_at DATETIME
 )ENGINE=InnoDB;
