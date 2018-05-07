d6ff9558afecff361758a3b301920f4010739947
Nicolas Streri <nicolasstreri@gmail.com>
2018-05-07 20:49:24 -0300

    Add FixtureFuncion

--- src/main/java/prode/Schedule.java
+++ src/main/java/prode/Schedule.java
@@ -6,4 +6,10 @@ public class Schedule extends Model {
 	static{
 		validatePresenceOf("nombre_fecha").message("Ingrese nombre del Equipo");
 	}
+
+	public Fixture obtenerFixturePerteneciente(){
+		Fixture temp = Fixture.findFirst("id=?", this.get("id_fixture"));
+		return temp;
+	}
+
 }
