47c664054f18f092f701a5aa0037d357f6477e97
Pablo <pabloivan018@gmail.com>
2018-05-06 21:25:26 -0300

    actualizacion base datos

--- src/migrations/20180506003614_create_playerfixture_table.sql
+++ src/migrations/20180506003614_create_playerfixture_table.sql
@@ -1,7 +1,8 @@
 create table playerfixtures (	
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	--id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	username_player varchar(128),
 	id_fixture INTEGER,
+	primary key (username_player,id_fixture),
 	CONSTRAINT control_player_suscripcion FOREIGN KEY (username_player) REFERENCES players(username),
 	CONSTRAINT control_fixture_suscripcion FOREIGN KEY (id_fixture) REFERENCES fixtures(id),
 	created_at DATETIME,
--- src/migrations/20180506021959_create_betresult_table.sql
+++ src/migrations/20180506021959_create_betresult_table.sql
@@ -1,8 +1,9 @@
 create table betresults( 
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	--id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	id_bet INTEGER,
 	id_result INTEGER,	
 	id_prediction INTEGER,
+	PRIMARY KEY(id_bet,id_result),
 	CONSTRAINT control_bet_bt FOREIGN KEY (id_bet) REFERENCES bets(id),
 	CONSTRAINT control_result_bt FOREIGN KEY (id_result) REFERENCES results(id),
 	CONSTRAINT control_prediction_bt FOREIGN KEY (id_prediction) REFERENCES predictions(id),
