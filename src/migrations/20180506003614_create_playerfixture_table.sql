e8d455f38dacdb63607c3a8688f2751ff68b372a
Pablo <pabloivan018@gmail.com>
2018-05-08 18:36:57 -0300

    Actualizacion DB playerfixture

--- src/migrations/20180506003614_create_playerfixture_table.sql
+++ src/migrations/20180506003614_create_playerfixture_table.sql
@@ -1,9 +1,9 @@
 create table players_fixtures (	
 	--id INTEGER AUTO_INCREMENT PRIMARY KEY,
-	username_player varchar(128),
+	player_username varchar(128),
 	fixture_id INTEGER,
-	primary key (username_player,fixture_id),
-	CONSTRAINT control_player_suscripcion FOREIGN KEY (username_player) REFERENCES players(username),
+	primary key (player_username,fixture_id),
+	CONSTRAINT control_player_suscripcion FOREIGN KEY (player_username) REFERENCES players(username),
 	CONSTRAINT control_fixture_suscripcion FOREIGN KEY (fixture_id) REFERENCES fixtures(id),
 	created_at DATETIME,
   	updated_at DATETIME
